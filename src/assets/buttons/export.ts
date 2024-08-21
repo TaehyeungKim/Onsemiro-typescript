b7bdfea3f8384e61ccc577ba16acdfebcfba984d
Taehyeungkim <kth1837@snu.ac.kr>
2024-08-22 03:14:41 +0900

    re-export button assets

---
+++ src/assets/buttons/export.ts
@@ -0,0 +1,4 @@
+export { default as Back } from "./arrow_left.png";
+export { default as Close } from "./ph_x.png";
+export { default as Menu } from "./hambg-menu.png";
+export { default as Login } from "./login.png";
