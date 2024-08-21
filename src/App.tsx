a4e2b348fe83abb0f0fa849307d9bcdd41f8af4f
Taehyeungkim <kth1837@snu.ac.kr>
2024-08-22 03:15:43 +0900

    :sparkles: signup route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "@/routes/Landing";
 import SignInPage from "@/routes/Signin";
+import SignUpPage from "@/routes/Signup";
 import AppFrame from "@/layout/AppFrame";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" Component={LandingPage} />
           <Route path="/signin" Component={SignInPage} />
+          <Route path="/signup" Component={SignUpPage} />
         </Routes>
       </Router>
     </AppFrame>
