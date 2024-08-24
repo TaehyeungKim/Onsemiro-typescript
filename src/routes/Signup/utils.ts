4276ba5e728d229f12b80d54429ad8db93505829
Taehyeungkim <kth1837@snu.ac.kr>
2024-08-24 14:34:02 +0900

    implemented some functions for signup data management

---
+++ src/routes/Signup/utils.ts
@@ -0,0 +1,55 @@
+import { SignUpClientStoreData } from "./type";
+
+export const executeOnDataFulfilled = (
+  level: number,
+  data: SignUpClientStoreData,
+  execute: () => void
+) => {
+  switch (level) {
+    case 0:
+      if (data.phoneVerification && data.schoolVerification && data.verifyCode)
+        execute();
+      break;
+    default:
+
+    // case 1:
+    //   if (data.kakao_id) execute();
+    //   break;
+    // case 2:
+    //   if (data.nickname) execute();
+    //   break;
+    // case 3:
+    //   if (data.gender_identity) execute();
+    //   break;
+    // case 4:
+    //   if (data.bdsm && data.gender_preference) execute();
+    //   break;
+    // case 5:
+    //   if (data.appearance && data.height && data.eyelid) execute();
+    //   break;
+    // case 6:
+    //   if (data.mbti && data.character) execute();
+    //   break;
+    // case 7:
+    //   if (data.interest && data.interest.length > 0) execute();
+    //   break;
+    // case 8:
+    //   if (data.meeting_frequency && data.city && data.subRegion) execute();
+    //   break;
+    // case 9:
+    //   if (data.std && data.photo) execute();
+    //   break;
+    // case 10:
+    //   if (data.introduction) execute();
+    //   break;
+    // case 11:
+    //   if (data.prefer_gender_identity) execute();
+    //   break;
+    // case 12:
+    //   execute();
+    //   break;
+    // case 13:
+    //   execute();
+    //   break;
+  }
+};
