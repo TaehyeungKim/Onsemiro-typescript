2fbf37599a75c72ebf4a4519cf6693b5fc31f3c6
Taehyeungkim <kth1837@snu.ac.kr>
2024-08-24 14:35:39 +0900

    added recoil root

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import LandingPage from "@/routes/Landing";
 import SignInPage from "@/routes/Signin";
 import SignUpPage from "@/routes/Signup";
 import AppFrame from "@/layout/AppFrame";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <AppFrame>
-      <Router>
-        <Routes>
-          <Route path="/" Component={LandingPage} />
-          <Route path="/signin" Component={SignInPage} />
-          <Route path="/signup" Component={SignUpPage} />
-        </Routes>
-      </Router>
-    </AppFrame>
+    <RecoilRoot>
+      <AppFrame>
+        <Router>
+          <Routes>
+            <Route path="/" Component={LandingPage} />
+            <Route path="/signin" Component={SignInPage} />
+            <Route path="/signup" Component={SignUpPage} />
+          </Routes>
+        </Router>
+      </AppFrame>
+    </RecoilRoot>
   );
 }
 
