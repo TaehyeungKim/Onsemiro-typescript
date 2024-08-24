5f232bc53eede5a0f288c86e42b73bb59abf6607
Taehyeungkim <kth1837@snu.ac.kr>
2024-08-24 14:35:59 +0900

    added api for signup

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,10 @@
 import { instance, instanceWithToken } from "./axios";
-import { SignInData } from "./type";
+import {
+  SignInData,
+  SignUpCheckVerifData,
+  SignUpPhoneVerifData,
+  SignUpSchoolVerifData,
+} from "./type";
 
 export const signIn = async (data: SignInData) => {
   const res = await instance.post("/account/signin/", data);
@@ -7,4 +12,31 @@ export const signIn = async (data: SignInData) => {
   if (res.status === 200) {
     return true;
   } else console.log("Error");
+};
+
+export const signUp = async (data: SignUpPhoneVerifData) => {
+  const response = await instance.post("/account/signup/", data);
+  if (response.status === 200 || response.status === 201) {
+    return true;
+  } else {
+    console.log("Error");
+  }
+  return false;
+};
+
+export const requestSchoolVerifyCode = async (data: SignUpSchoolVerifData) => {
+  const response = await instanceWithToken.post("/account/email/", data);
+  if (response.status === 200 || response.status === 201) {
+    return true;
+  } else {
+    console.log("Error");
+  }
+  return false;
+};
+
+//dummy
+export const checkSchoolVerifyCode = async (data: SignUpCheckVerifData) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(true), 1000);
+  });
 };
--- src/api/type.ts
+++ src/api/type.ts
@@ -1,3 +1,16 @@
 export type SignInData = {
   phone_num: string;
 };
+
+export type SignUpPhoneVerifData = {
+  phone_num: string;
+};
+
+export type SignUpSchoolVerifData = {
+  univ: string;
+  email: string;
+};
+
+export type SignUpCheckVerifData = {
+  code: string;
+};
